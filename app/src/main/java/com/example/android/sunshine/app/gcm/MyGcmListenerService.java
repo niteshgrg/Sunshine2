cdf74cb36c6b01a90bdf4d054851d02ba1466a70
niteshgrg <ngarg@fybr-tech.com>
2016-03-07 15:58:09 +0530

    added system build.gradle

--- app/src/main/java/com/example/android/sunshine/app/gcm/MyGcmListenerService.java
+++ app/src/main/java/com/example/android/sunshine/app/gcm/MyGcmListenerService.java
@@ -1,6 +1,5 @@
 package com.example.android.sunshine.app.gcm;
 
-import android.app.ActivityManager;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -77,16 +76,6 @@ public class MyGcmListenerService extends GcmListenerService {
                         .setPriority(NotificationCompat.PRIORITY_HIGH);
         mBuilder.setContentIntent(contentIntent);
         mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
-    }
-
-    public boolean isMyServiceRunning(Class<?> serviceClass) {
-        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
-            if (serviceClass.getName().equals(service.service.getClassName())) {
-                return true;
-            }
-        }
-        return false;
     }
 }
 
